bbf0bc53969e2b26a3980d72e52faf760f3d4aa6
Romain Da Silva Faria <romaindsf@hotmail.fr>
2024-06-07 16:13:24 +0200

    chat positionned correctly

--- src/app/[streamerName]/page.js
+++ src/app/[streamerName]/page.js
@@ -1,7 +1,30 @@
 'use client'
 import { useParams } from 'next/navigation'
+import styles from './_streamerName.module.scss'
+import data from '../../data/data.json'
+import { useEffect } from 'react'
 
 export default function StreamerName() {
   const { streamerName } = useParams()
-  return <h1>{streamerName}</h1>
+  const allChannels = [...data.Followed_channels, ...data.Recommended_channels]
+  const streamerInfo = allChannels.find(
+    (streamer) => streamer.name === streamerName
+  )
+  useEffect(() => console.log(streamerInfo))
+  return (
+    <div>
+      <div className={styles.main}>
+        <h1>{streamerName}</h1>
+        <div className={styles.video_div}>
+          <h3>VIDEO KILLED THE RADIOSTAR</h3>
+        </div>
+        <h2>{streamerInfo.title}</h2>
+        <div className={styles.description_div}>
+          <p>joue acuellement à {streamerInfo.category}</p>
+          <p>{streamerInfo.nbViewers}</p>
+        </div>
+      </div>
+      <div className={styles.chat}></div>
+    </div>
+  )
 }
